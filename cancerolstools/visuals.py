6d389a0f4a7a5d2c78ccc396b9caa67bc2218015
Noah Tran <noah.s.tran@berkeley.edu>
2023-05-09 06:02:20 +0000

    various fixes on visuals package + refactor using vi

--- cancerolstools/visuals.py
+++ cancerolstools/visuals.py
@@ -40,12 +40,12 @@ def get_variable_group(variable):
     else:
         return 'Other'
     
-def anomaly_map():
+def anomaly_map(plotdf):
     """
     Creates plotly express map of anomalies.
     
     inputs:
-        None
+        plotdf: result of pp.graph_dataframe()
     
     outputs:
         fig object with plot
@@ -61,30 +61,40 @@ def anomaly_map():
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
 
-def race_income_plot():
+def race_income_plot(df):
     """
     Creates faceted plot of race and income effect on cancer death rate.
     
     inputs:
-        None
+        df: result of df_race()
     
     outputs:
         fig object with plot
     """
-    df_race = cancer[['TARGET_deathRate', 'medIncome', 'PctWhite', 'PctBlack', 'PctAsian', 'PctOtherRace']]
-    df_race = df_race.melt(id_vars=['TARGET_deathRate', 'medIncome'], var_name='variable')
-    df_race['variable_group'] = df_race['variable'].apply(get_variable_group)
-    
-    
     colors = ['red', 'blue', 'green', 'purple']
-    color_map = dict(zip(df_race['variable_group'].unique(), colors))
-
+    color_map = dict(zip(df['variable_group'].unique(), colors))
+    
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 12))
-    for ax, variable in zip(axes.flatten(), df_race['variable'].unique()):
-        data = df_race[df_race['variable'] == variable]
+    for ax, variable in zip(axes.flatten(), df['variable'].unique()):
+        data = df[df['variable'] == variable]
         ax.scatter(data['value'], data['TARGET_deathRate'], c=data['variable_group'].apply(lambda x: color_map[x]), s=data['medIncome']/5000, alpha=0.7)
         ax.set_xlabel('Percentage of population by race')
         ax.set_ylabel('Target death rate')
         ax.set_title(variable)
         ax.set_ylim([100, 400])
     return fig
+
+def df_race(cancer_df):
+    """
+    Returns plot-ready dataframe for racial differences in cancer death rates.
+    
+    inputs:
+        df: merged cancer dataframe
+    
+    outputs:
+        Pandas dataframe
+    """
+    df = cancer_df[['TARGET_deathRate', 'medIncome', 'PctWhite', 'PctBlack', 'PctAsian', 'PctOtherRace']]
+    df = df.melt(id_vars=['TARGET_deathRate', 'medIncome'], var_name='variable')
+    df['variable_group'] = df['variable'].apply(get_variable_group)
+    return df
